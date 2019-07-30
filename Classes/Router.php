3c87bfdcf518350cd279e150871abc2ff819aa87
George Gulidov <george.gulidov@rotary.org>
2019-07-30 12:28:29 -0500

    apikey moved to headers

--- Classes/Router.php
+++ Classes/Router.php
@@ -9,11 +9,15 @@ class Router
     private $routes = [];
     private $controller;
     private $api_routes_id = [];
+    private $api_key = null;
 
     public function __construct(Controller $controller)
     {
         $this->uri = $_SERVER['REQUEST_URI'];
         $this->controller = $controller;
+        if (!empty($_SERVER['HTTP_FINANCE_KEY'])) $this->api_key = $_SERVER['HTTP_FINANCE_KEY'];
+
+
     }
 
     public function addAPIRoute($route, $action)
@@ -21,11 +25,11 @@ class Router
         if (strpos($route, '{ID}')){
             $route = str_replace('/', '\/', $route);
             $route = str_replace('{ID}', '(.*)', $route);
-            $route = '/^\/(.*)' . $route . '$/';
+            $route = '/^' . $route . '$/';
             $this->api_routes_id[$route] = $action;
         } else {
             $route = str_replace('/', '\/', $route);
-            $route = '/^\/(.*)' . $route . '$/';
+            $route = '/^' . $route . '$/';
             $this->api_routes[$route] = $action;
         }
 
@@ -42,16 +46,15 @@ class Router
     private function parseRoute()
     {
         foreach ($this->api_routes_id as $rt => $act) {
-            if (preg_match($rt, $this->uri, $matches)) {
+            if (preg_match($rt, $this->uri, $matches) && $this->api_key) {
                 Logger::log('Incoming: ' . $this->uri);
-                $this->controller->{$act}(new Request($matches[1], $matches[2]));
+                $this->controller->{$act}(new Request($this->api_key, $matches[1]));
                 return;
             }
         }
         foreach ($this->api_routes as $route => $action) {
-            if (preg_match($route, $this->uri, $matches)) {
-                Logger::log('Incoming: ' . $this->uri);
-                $this->controller->{$action}(new Request($matches[1]));
+            if (preg_match($route, $this->uri) && $this->api_key) {
+                $this->controller->{$action}(new Request($this->api_key));
                 return;
             }
         }
